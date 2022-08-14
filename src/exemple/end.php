b44df5b84be841dda683e69d12e518037d5970bb
Patrick PREMARTIN <patrick@premartin.fr>
2022-08-14 10:48:50 +0200

    sample program

---
+++ src/exemple/end.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+	<p>Traitement terminé.</p>
+	<p><img src="exemple-recadre.jpg"></p>
+	<p><a href="start.php">Recommencer</a></p>
+</body>
+</html>
---
+++ src/exemple/start.php
@@ -0,0 +1,4 @@
+<?php
+	require_once(dirname(__FILE__)."/../recadrer-image/recadrer-image.php");
+	Recadrer_Image(dirname(__FILE__)."/exemple.jpg","exemple.jpg",dirname(__FILE__)."/exemple-recadre.jpg","../exemple/end.php",180,180,100,100,"../recadrer-image");
+?>
